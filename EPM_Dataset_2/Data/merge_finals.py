9122e7a2f6b824451cbf69a08703ee42871d57e8
Dante Bencivenga <drbenciv@ucalgary.ca>
2022-11-30 17:10:25 -0700

    add comments to merging code

--- EPM_Dataset_2/Data/merge_finals.py
+++ EPM_Dataset_2/Data/merge_finals.py
@@ -1,16 +1,15 @@
 with open("final_grades_first.csv", 'r') as first, open("final_grades_second.csv", 'r') as second, open("final_grades_merged.csv", 'w') as merged:
-  merged.write(first.readline())
-  merged.write(first.readline())
-  second.readline(); second.readline()
+  merged.write(first.readline()) # header line 1
+  merged.write(first.readline()) # header line 2
+  second.readline(); second.readline() # don't duplicate headers
   line1 = first.readline()
-  id1 = int(line1[:line1.find(',')])
+  id1 = int(line1[:line1.find(',')]) # extract ID number from line
   line2 = second.readline()
   id2 = int(line2[:line2.find(',')])
-  while True:
-    if id1 == id2 == 1000: break
+  while id1 < 1000 or id2 < 1000: # 1000 = finished reading
     if id2 <= id1:
-      if id2 < id1: merged.write(line2)
-      line2 = second.readline()
+      if id2 < id1: merged.write(line2) # if a student wrote both exams, don't record the second one
+      line2 = second.readline() # move on to next student
       id2 = int(line2[:line2.find(',')]) if line2 else 1000
     else:
       merged.write(line1)
